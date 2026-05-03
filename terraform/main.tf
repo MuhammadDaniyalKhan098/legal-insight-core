654c1d3de5136e53919a7dab7947faab7c86aeb0
MuhammadDaniyalKhan098 <26100266@lums.edu.pk>
2026-05-03 23:19:57 +0500

    Testing CI/CD ignore paths

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,3 +86,6 @@ output "server_public_ip" {
   value       = aws_instance.legal_insight_server.public_ip
   description = "The public IP address of the EC2 instance"
 }
+
+
+# Testing CI/CD ignore paths
